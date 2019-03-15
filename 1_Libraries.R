9585eee42b549d659773deed27f4359cec6445e0
Aurore Paligot <aurorepaligot@hotmail.com>
2019-03-15 12:20:32 +0100

    Add templates
    Add R script templates - empty

---
+++ 1_Libraries.R
@@ -0,0 +1,19 @@
+##_______________________________________________________________________________________________________##
+##
+##
+##
+##  Project Name :
+##  Developer Name : Aurore Paligot
+##  
+##  Script Name : 1_Libraries
+##  Description : List of Libraries needed for the project
+##  Current Status :
+##  Date of 1st Release :
+##  History versions : 
+##
+##  Outputs : 
+##
+##  Related Scripts : 
+##
+##                                                                                                       
+##_______________________________________________________________________________________________________##  
---
+++ 2_PreProcessing.R
@@ -0,0 +1,19 @@
+##_______________________________________________________________________________________________________##
+##
+##
+##
+##  Project Name :
+##  Developer Name : Aurore Paligot
+##  
+##  Script Name : 2_PreProcessing
+##  Description :
+##  Current Status :
+##  Date of 1st Release :
+##  History versions : 
+##
+##  Outputs : 
+##
+##  Related Scripts : 
+##
+##                                                                                                       
+##_______________________________________________________________________________________________________##  
