3eb127c56696da7552d405695a005bfd34bc9c28
Aurore Paligot <aurorepaligot@hotmail.com>
2019-03-28 13:48:05 +0100

    Update

--- 1_Libraries.R
+++ 1_Libraries.R
@@ -2,13 +2,13 @@
 ##
 ##
 ##
-##  Project Name :
+##  Project Name : LSFB Dashboard
 ##  Developer Name : Aurore Paligot
 ##  
 ##  Script Name : 1_Libraries
 ##  Description : List of Libraries needed for the project
-##  Current Status :
-##  Date of 1st Release :
+##  Current Status :in process
+##  Date of 1st Release :15-03-2019
 ##  History versions : 
 ##
 ##  Outputs : 
